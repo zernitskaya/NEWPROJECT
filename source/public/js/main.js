c6aa0c324fe2d21d242c60adb792b7412c2ee9c7
dashazernitskaya <zernitskaya@icloud.com>
2021-12-27 20:41:01 +0200

    added new

---
+++ source/public/js/main.js
@@ -0,0 +1,15 @@
+ const aside = document.querySelector('.aside');
+
+aside.addEventListener('click', (e) => {
+  e.stopPropagation()
+  const thisEl = e.target;
+
+  if (thisEl.classList.contains('nav__open-sub-menu')) {
+    thisEl.parentElement.classList.toggle('nav__item--active');
+  }
+
+  if (thisEl.classList.contains('burger-btn')) {
+    aside.classList.toggle('active');
+  }
+});
+
